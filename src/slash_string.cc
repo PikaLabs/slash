345e1f9f8e4723488baba0a289dc78525f3ccb8d
baotiao <baotiao@gmail.com>
2017-02-08 19:27:45 +0800

    add support for gcc 5

--- src/slash_string.cc
+++ src/slash_string.cc
@@ -415,9 +415,9 @@ int string2l(const char *s, size_t slen, long *lval) {
 /* Convert a double to a string representation. Returns the number of bytes
  * required. The representation should always be parsable by strtod(3). */
 int d2string(char *buf, size_t len, double value) {
-    if (isnan(value)) {
+    if (std::isnan(value)) {
         len = snprintf(buf,len,"nan");
-    } else if (isinf(value)) {
+    } else if (std::isinf(value)) {
         if (value < 0)
             len = snprintf(buf,len,"-inf");
         else
