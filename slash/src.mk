02bb7abbf1e204b18d699f192c0ec7f886703a1a
gaodunqiao <gaodq7c7+github@gmail.com>
2017-07-14 13:44:17 +0800

    Update Makefile

---
+++ slash/src.mk
@@ -0,0 +1,23 @@
+LIB_SOURCES = \
+	src/base_conf.cc \
+	src/cond_lock.cc \
+	src/env.cc \
+	src/posix.cc \
+	src/rsync.cc \
+	src/slash_binlog_impl.cc \
+	src/slash_coding.cc \
+	src/slash_hash.cc \
+	src/slash_mutex.cc \
+	src/slash_status.cc \
+	src/slash_string.cc \
+	src/slash_testharness.cc \
+	src/testutil.cc \
+	src/build_version.cc \
+
+EXAMPLE_SOURCE = \
+	examples/binlog_example.cc \
+	examples/cond_lock_example.cc \
+	examples/conf_example.cc \
+	examples/hash_example.cc \
+	examples/mutex_example.cc \
+
